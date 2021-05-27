70cd70c51b8b7c8f0f86822dbfb356fe0abe7a5b
guilherme <jguicf@gmail.com>
2021-05-26 20:26:24 -0400

    fix: inserirFila

--- consultorio.c
+++ consultorio.c
@@ -573,14 +573,14 @@ Fila* criarFila(){
 
 }
 
-void inserirFila(Fila* fila, char nome[], char dtNasc[], char situacao, int totalSessoes, int qtdFaltas, int faltasConsecutivas){
+void inserirFila(Fila* fila, Paciente* p){
     Paciente* novoPaciente = (Paciente*) malloc(sizeof(Paciente));
-    strcpy(novoPaciente->nome,nome);
-    strcpy(novoPaciente->dtNascimento,dtNasc);
-    novoPaciente->situacao = situacao;
-    novoPaciente->totalSessoes = totalSessoes;
-    novoPaciente->qtdFaltas = qtdFaltas;
-    novoPaciente->faltasConsecutivas = faltasConsecutivas;
+    strcpy(novoPaciente->nome,p->nome);
+    strcpy(novoPaciente->dtNascimento,novoPaciente->dtNascimento);
+    novoPaciente->situacao = novoPaciente->situacao;
+    novoPaciente->totalSessoes = novoPaciente->totalSessoes;
+    novoPaciente->qtdFaltas = novoPaciente->qtdFaltas;
+    novoPaciente->faltasConsecutivas = novoPaciente->faltasConsecutivas;
     
     
     if(fila->inicio == NULL){
